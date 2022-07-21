2e196a13549a577b1d27cd2b17e8dbe9ffb9d7ab
gaoyafei <gaoyafei@luojilab.com>
2022-07-21 08:51:16 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Three.js 学习
+
+学习地址：
+
+- [Three.js 官方文档](https://threejs.org/docs/index.html#manual/zh/introduction/Creating-a-scene)
+- [Bruno Simon three.js journey: 官网](https://threejs-journey.com)
+- [Bruno Simon three.js journey: B 站](https://www.bilibili.com/video/BV1wY4y1h765)
+
+项目说明：
+
+- 使用 Pnpm Workspace 构建 Monorepo（单仓库多项目）
+-
---
+++ packages/lessons4/src/main.ts
@@ -0,0 +1,34 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Scene
+const scene = new THREE.Scene();
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+
+// Canvas
+const canvas = document.querySelector('canvas#webgl')!;
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 3;
+
+renderer.render(scene, camera);
