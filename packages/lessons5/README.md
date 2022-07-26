1d24138687c8b08566263752925bab7a34be0a1c
Vincent <vincent.xj@outlook.com>
2022-07-26 23:26:41 +0800

    feat: 新增 旋转的绿色虚线立方体 demo

--- packages/lessons5/README.md
+++ packages/lessons5/README.md
@@ -1,3 +1,10 @@
 # Lessons 5
 
-> 常用材质：点材质、线材质（虚线材质不生效）
+[虚线材质(LineDashedMaterial)](https://threejs.org/docs/index.html#api/zh/materials/LineDashedMaterial)
+
+WebGL / lines / dashed
+
+- [示例](https://threejs.org/examples/#webgl_lines_dashed)
+- [代码](https://github.com/mrdoob/three.js/blob/master/examples/webgl_lines_dashed.html)
+
+> 旋转的绿色虚线立方体
--- packages/lessons5/src/main.ts
+++ packages/lessons5/src/main.ts
@@ -1,39 +1,41 @@
 import './style.css';
 import {
+  BufferGeometry,
+  Float32BufferAttribute,
+  LineDashedMaterial,
+  LineSegments,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer,
-  SphereGeometry,
-  PointsMaterial,
-  Points,
-  LineBasicMaterial,
-  LineDashedMaterial,
-  Line
+  WebGLRenderer
 } from 'three';
 
 let camera: PerspectiveCamera, scene: Scene, renderer: WebGLRenderer;
 
+let cube: LineSegments;
+
 init();
-render();
+animate();
 
 function init() {
   const { innerWidth, innerHeight } = window;
 
   // Camera
-  camera = new PerspectiveCamera(45, innerWidth / innerHeight, 1, 1000);
-  camera.position.set(0, 0, 500);
-  camera.lookAt(0, 0, 0);
+  camera = new PerspectiveCamera(60, innerWidth / innerHeight, 1, 500);
+  camera.position.set(0, 0, 100);
 
   // Scene
   scene = new Scene();
 
   // Object
-  // 点材质 PointsMaterial
-  addPointsMaterial();
-  // 基础线材质 LineBasicMaterial
-  addLineBasicMaterial();
-  // 虚线材质 LineDashedMaterial
-  addLineDashedMaterial();
+  const geometry = boxGeometry(50, 50, 50);
+  const material = new LineDashedMaterial({
+    color: 0x00ff00,
+    dashSize: 3,
+    gapSize: 1
+  });
+  cube = new LineSegments(geometry, material);
+  cube.computeLineDistances();
+  scene.add(cube);
 
   // Renderer
   const canvas = document.querySelector('canvas#webgl')!;
@@ -44,47 +46,104 @@ function init() {
   window.addEventListener('resize', onWindowResize);
 }
 
-function addPointsMaterial() {
-  // 球体几何对象
-  const geometry = new SphereGeometry(100, 25, 25);
-  // 点材质对象
-  const material = new PointsMaterial({
-    color: 0xff0000,
-    size: 3 // 点渲染尺寸
-  });
-  // 点模型对象
-  const point = new Points(geometry, material);
-  point.position.x = -220;
-  scene.add(point);
-}
+function boxGeometry(
+  width: number,
+  height: number,
+  depth: number
+): BufferGeometry {
+  width = width * 0.5;
+  height = height * 0.5;
+  depth = depth * 0.5;
 
-function addLineBasicMaterial() {
-  // 球体几何对象
-  const geometry = new SphereGeometry(100, 25, 25);
-  // 直线基础材质对象
-  const material = new LineBasicMaterial({
-    color: 0x00ff00
-  });
-  // 线模型对象
-  const line = new Line(geometry, material);
-  scene.add(line);
-}
+  const position = [
+    -width,
+    -height,
+    -depth,
+    -width,
+    height,
+    -depth,
 
-function addLineDashedMaterial() {
-  // 球体几何对象
-  const geometry = new SphereGeometry(100, 25, 25);
-  // 虚线材质对象
-  const material = new LineDashedMaterial({
-    color: 0x0000ff,
-    dashSize: 10, // 显示线段的大小。默认为 3。
-    gapSize: 5 // 间隙的大小。默认为 1
-  });
-  // 线模型对象
-  const line = new Line(geometry, material);
-  // 计算 LineDashedMaterial 所需的距离数组
-  line.computeLineDistances();
-  line.position.x = 220;
-  scene.add(line);
+    -width,
+    height,
+    -depth,
+    width,
+    height,
+    -depth,
+
+    width,
+    height,
+    -depth,
+    width,
+    -height,
+    -depth,
+
+    width,
+    -height,
+    -depth,
+    -width,
+    -height,
+    -depth,
+
+    -width,
+    -height,
+    depth,
+    -width,
+    height,
+    depth,
+
+    -width,
+    height,
+    depth,
+    width,
+    height,
+    depth,
+
+    width,
+    height,
+    depth,
+    width,
+    -height,
+    depth,
+
+    width,
+    -height,
+    depth,
+    -width,
+    -height,
+    depth,
+
+    -width,
+    -height,
+    -depth,
+    -width,
+    -height,
+    depth,
+
+    -width,
+    height,
+    -depth,
+    -width,
+    height,
+    depth,
+
+    width,
+    height,
+    -depth,
+    width,
+    height,
+    depth,
+
+    width,
+    -height,
+    -depth,
+    width,
+    -height,
+    depth
+  ];
+  const attribute = new Float32BufferAttribute(position, 3);
+  const geometry = new BufferGeometry().setAttribute('position', attribute);
+
+  return geometry;
 }
 
 function onWindowResize() {
@@ -94,6 +153,17 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
 
   renderer.setSize(innerWidth, innerHeight);
+
+  render();
+}
+
+function animate() {
+  const time = Date.now() * 0.001;
+
+  requestAnimationFrame(animate);
+
+  cube.rotation.x = 0.25 * time;
+  cube.rotation.y = 0.25 * time;
 
   render();
 }
