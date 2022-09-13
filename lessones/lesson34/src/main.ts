77965711b951e8a02c3bbb665eb43744ce93bbca
Vincent <vincent.xj@outlook.com>
2022-09-13 23:16:24 +0800

    feat: 新增语音模块

---
+++ lessones/lesson34/src/main.ts
@@ -0,0 +1,201 @@
+import './style.css';
+import {
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  sRGBEncoding,
+  AudioListener,
+  Audio,
+  AudioLoader,
+  TextureLoader,
+  SpriteMaterial,
+  Sprite,
+  Clock,
+  AxesHelper,
+  PositionalAudio,
+  MeshBasicMaterial,
+  BoxGeometry,
+  Group,
+  Mesh,
+  AudioAnalyser
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import steelBall from '@/textures/sprite.png?url';
+import backgroundMusic from '@/sounds/376737_Skullbeatz___Bad_Cat_Maste.mp3?url';
+import ballMusic from '@/sounds/Project_Utopia.mp3?url';
+
+let camera: PerspectiveCamera, scene: Scene, renderer: WebGLRenderer;
+
+let ball: Sprite;
+
+let group: Group, analyser: AudioAnalyser;
+
+const clock = new Clock();
+
+init();
+animate();
+
+function init() {
+  const { innerWidth, innerHeight, devicePixelRatio } = window;
+
+  // Canera
+  camera = new PerspectiveCamera(50, innerWidth / innerHeight, 1, 500);
+  camera.position.z = 10;
+
+  // Scene
+  scene = new Scene();
+
+  // Axes
+  const axesHelper = new AxesHelper(300);
+  scene.add(axesHelper);
+
+  // Object
+  addBall();
+  // addAudio();
+  // addPositionalAudio();
+  addAudioVisualizer();
+
+  // Renderer
+  const canvas = document.querySelector('canvas#webgl')!;
+  renderer = new WebGLRenderer({ canvas });
+  renderer.setSize(innerWidth, innerHeight);
+  renderer.setPixelRatio(devicePixelRatio);
+  renderer.outputEncoding = sRGBEncoding;
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 1;
+  controls.maxDistance = 500;
+  controls.update();
+
+  // Resize
+  window.addEventListener('resize', onWindowResize);
+}
+
+function addBall() {
+  const texture = new TextureLoader().load(steelBall);
+  const material = new SpriteMaterial({ map: texture });
+  ball = new Sprite(material);
+  scene.add(ball);
+}
+
+// 非位置音频，可用于不考虑位置的背景音乐
+function addAudio() {
+  // 创建一个监听者
+  const listener = new AudioListener();
+  // 监听者绑定到相机对象
+  camera.add(listener);
+
+  // 创建一个非位置音频对象，用来控制播放
+  const mainAudio = new Audio(listener);
+
+  // 创建一个音频加载器对象
+  const audioLoader = new AudioLoader();
+  // 加载音频文件，返回一个音频缓冲区对象作为回调函数参数
+  audioLoader.load(backgroundMusic, (buffer) => {
+    // AudioBuffer 音频缓冲区对象关联到音频对象 mainAudio
+    mainAudio.setBuffer(buffer);
+    mainAudio.setLoop(true); // 循环
+    mainAudio.setVolume(0.1); // 音量
+
+    // 播放缓冲区中的音频数据
+    mainAudio.play(); // play 播放、stop 停止、pause 暂停
+  });
+}
+
+function addPositionalAudio() {
+  // 创建一个监听者
+  const listener = new AudioListener();
+
+  // 创建一个位置音频对象，监听者作为参数，音频和监听者关联。
+  const ballAudio = new PositionalAudio(listener);
+  // 音源绑定到一个网格模型上
+  ball.add(ballAudio);
+
+  // 创建一个音频加载器对象
+  const audioLoader = new AudioLoader();
+  // 加载音频文件，返回一个音频缓冲区对象作为回调函数参数
+  audioLoader.load(ballMusic, (buffer) => {
+    // AudioBuffer 音频缓冲区对象关联到音频对象 ballAudio
+    ballAudio.setBuffer(buffer);
+    ballAudio.setLoop(true); // 循环
+    ballAudio.setVolume(0.9); // 音量
+    ballAudio.setRefDistance(200); //参数值越大,声音越大
+
+    // 播放缓冲区中的音频数据
+    ballAudio.play(); // play 播放、stop 停止、pause 暂停
+  });
+}
+
+function addAudioVisualizer() {
+  /**
+   * 创建多个网格模型组成的组对象
+   */
+  group = new Group();
+  const COUNT = 128; // 控制音频分析器返回频率数据数量
+
+  for (let i = 0; i < COUNT / 2; i++) {
+    const box = new BoxGeometry(1, 10, 10); //创建一个立方体几何对象
+    const material = new MeshBasicMaterial({
+      color: 0x00ff00
+    }); //材质对象
+    const mesh = new Mesh(box, material); //网格模型对象
+    // 长方体间隔 2，整体居中
+    mesh.position.set(2 * i - (COUNT / 2) * 1, 0, 0);
+    group.add(mesh);
+  }
+  scene.add(group);
+
+  const listener = new AudioListener(); // 创建一个监听者
+  camera.add(listener); // 监听者绑定到相机对象
+  const audio = new Audio(listener); // 非位置音频对象
+  const audioLoader = new AudioLoader(); // 音频加载器
+  // 加载音频文件
+  audioLoader.load(backgroundMusic, (buffer) => {
+    audio.setBuffer(buffer); // 音频缓冲区对象关联到音频对象 audio
+    audio.setLoop(true); // 循环
+    audio.setVolume(0.5); // 音量
+    audio.play(); // 播放
+
+    // 音频分析器和音频绑定，可以实时采集音频时域数据进行快速傅里叶变换
+    analyser = new AudioAnalyser(audio, 2 * COUNT);
+    console.log('🌈 analyser:', analyser.getFrequencyData());
+  });
+}
+
+function onWindowResize() {
+  const { innerWidth, innerHeight } = window;
+
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(innerWidth, innerHeight);
+
+  render();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const elapsedTime = clock.getElapsedTime();
+
+  ball.position.set(5 * Math.sin(elapsedTime), 0, 10 * Math.cos(elapsedTime));
+
+  if (analyser) {
+    // 获得频率数据N个
+    const arr = analyser.getFrequencyData();
+    // console.log('🌈 arr:', arr);
+    // 遍历组对象，每个网格子对象设置一个对应的频率数据
+    group.children.forEach((elem, index) => {
+      elem.scale.y = arr[index] / 8;
+      ((elem as Mesh).material as MeshBasicMaterial).color.r = arr[index] / 200;
+    });
+  }
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
---
+++ lessones/lesson34/src/textures.ts
@@ -0,0 +1,60 @@
+import bridge1 from '@/textures/bridge/px.jpg?url';
+import bridge2 from '@/textures/bridge/nx.jpg?url';
+import bridge3 from '@/textures/bridge/py.jpg?url';
+import bridge4 from '@/textures/bridge/ny.jpg?url';
+import bridge5 from '@/textures/bridge/pz.jpg?url';
+import bridge6 from '@/textures/bridge/nz.jpg?url';
+
+import castle1 from '@/textures/castle/px.jpg?url';
+import castle2 from '@/textures/castle/nx.jpg?url';
+import castle3 from '@/textures/castle/py.jpg?url';
+import castle4 from '@/textures/castle/ny.jpg?url';
+import castle5 from '@/textures/castle/pz.jpg?url';
+import castle6 from '@/textures/castle/nz.jpg?url';
+
+import park1 from '@/textures/park/px.jpg?url';
+import park2 from '@/textures/park/nx.jpg?url';
+import park3 from '@/textures/park/py.jpg?url';
+import park4 from '@/textures/park/ny.jpg?url';
+import park5 from '@/textures/park/pz.jpg?url';
+import park6 from '@/textures/park/nz.jpg?url';
+
+import pisa1 from '@/textures/pisa/px.png?url';
+import pisa2 from '@/textures/pisa/nx.png?url';
+import pisa3 from '@/textures/pisa/py.png?url';
+import pisa4 from '@/textures/pisa/ny.png?url';
+import pisa5 from '@/textures/pisa/pz.png?url';
+import pisa6 from '@/textures/pisa/nz.png?url';
+
+import sky1 from '@/textures/sky/px.jpg?url';
+import sky2 from '@/textures/sky/nx.jpg?url';
+import sky3 from '@/textures/sky/py.jpg?url';
+import sky4 from '@/textures/sky/ny.jpg?url';
+import sky5 from '@/textures/sky/pz.jpg?url';
+import sky6 from '@/textures/sky/nz.jpg?url';
+
+import star1 from '@/textures/star/px.jpg?url';
+import star2 from '@/textures/star/nx.jpg?url';
+import star3 from '@/textures/star/py.jpg?url';
+import star4 from '@/textures/star/ny.jpg?url';
+import star5 from '@/textures/star/pz.jpg?url';
+import star6 from '@/textures/star/nz.jpg?url';
+
+interface TextureMap {
+  bridge: Array<string>;
+  castle: Array<string>;
+  park: Array<string>;
+  pisa: Array<string>;
+  sky: Array<string>;
+  star: Array<string>;
+  [key: string]: Array<string>;
+}
+// 材质贴图路径
+export const textureMap: TextureMap = {
+  bridge: [bridge1, bridge2, bridge3, bridge4, bridge5, bridge6],
+  castle: [castle1, castle2, castle3, castle4, castle5, castle6],
+  park: [park1, park2, park3, park4, park5, park6],
+  pisa: [pisa1, pisa2, pisa3, pisa4, pisa5, pisa6],
+  sky: [sky1, sky2, sky3, sky4, sky5, sky6],
+  star: [star1, star2, star3, star4, star5, star6]
+};
