057c1233bda8ea7ddfaa5d11da311fad2bd96cf0
Vincent <vincent.xj@outlook.com>
2022-10-18 13:32:18 +0800

    feat: 优化 3D 字体 demo

--- lessones/lesson03/src/main.ts
+++ lessones/lesson03/src/main.ts
@@ -1,31 +1,33 @@
-import "./style.css";
+import './style.css';
 import {
   OrthographicCamera,
   Scene,
-  PointLight,
   WebGLRenderer,
-  MeshPhongMaterial,
   Mesh,
-  PointLightHelper,
   AxesHelper,
   CameraHelper,
-} from "three";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
+  TextureLoader,
+  MeshMatcapMaterial,
+  TorusGeometry,
+  Clock
+} from 'three';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
-import helvetikerRegular from "@/fonts/helvetiker_regular.typeface.json?url";
+import matcapsUrl from '@/textures/matcaps/8.png?url';
+import helvetikerRegularUrl from '@/fonts/helvetiker_regular.typeface.json?url';
 
 let camera: OrthographicCamera,
   scene: Scene,
-  pointLight: PointLight,
   renderer: WebGLRenderer,
-  stats: Stats;
+  stats: Stats,
+  controls: OrbitControls;
 
-let cameraZoom = 250; // 三维场景显示范围控制系数，系数越大，显示的范围越大
+let cameraZoom = 5; // 三维场景显示范围控制系数，系数越大，显示的范围越大
 
-let time = 0;
+const clock = new Clock();
 
 init();
 animate();
@@ -44,63 +46,79 @@ function init() {
     cameraZoom * aspectRatio,
     cameraZoom,
     -cameraZoom,
-    1,
-    2000
+    0.1,
+    100
   );
-  camera.position.set(0, 0, 1500);
+  camera.position.set(0, 0, 10);
 
-  const cameraHelper = new CameraHelper(camera);
-  scene.add(cameraHelper);
-
-  // Light
-  pointLight = new PointLight(0xffffff, 1.5);
-  pointLight.position.set(-350, 100, 90);
-  scene.add(pointLight);
-
-  const pointLightHelper = new PointLightHelper(pointLight, 10);
-  scene.add(pointLightHelper);
+  // const cameraHelper = new CameraHelper(camera);
+  // scene.add(cameraHelper);
 
   // Axes
-  const axesHelper = new AxesHelper(500);
-  scene.add(axesHelper);
+  // const axesHelper = new AxesHelper(200);
+  // scene.add(axesHelper);
 
   // Renderer
-  const canvas = document.querySelector("canvas#webgl")!;
+  const canvas = document.querySelector('canvas#webgl')!;
   renderer = new WebGLRenderer({ canvas, antialias: true });
   renderer.setSize(innerWidth, innerHeight);
   renderer.setPixelRatio(devicePixelRatio);
 
+  // Textures
+  const textureLoader = new TextureLoader();
+  const matcapTexture = textureLoader.load(matcapsUrl);
+
   // Object
-  const loader = new FontLoader();
-  loader.load(helvetikerRegular, (font) => {
-    const textGeometry = new TextGeometry("Hello, three.js!", {
+  const fontLoader = new FontLoader();
+  fontLoader.load(helvetikerRegularUrl, (font) => {
+    // 贴图
+    const material = new MeshMatcapMaterial();
+    material.matcap = matcapTexture;
+
+    // 字体
+    const textGeometry = new TextGeometry('Hello, three.js!', {
       font,
-      size: 80,
-      height: 5,
+      size: 0.8,
+      height: 0.3,
       curveSegments: 12,
       bevelEnabled: true,
-      bevelThickness: 10,
-      bevelSize: 4,
-      bevelSegments: 5,
+      bevelThickness: 0.03,
+      bevelSize: 0.02,
+      bevelOffset: 0,
+      bevelSegments: 5
     });
-    const textMaterials = new MeshPhongMaterial({ color: 0x00ffff });
-    const textMesh = new Mesh(textGeometry, textMaterials);
-    textMesh.position.set(-350, 0, 0);
-    scene.add(textMesh);
+    textGeometry.center();
+    const text = new Mesh(textGeometry, material);
+    scene.add(text);
+
+    // 甜甜圈
+    const donutGeometry = new TorusGeometry(0.3, 0.2, 32, 64);
+    for (let i = 0; i < 100; i++) {
+      const donut = new Mesh(donutGeometry, material);
+      donut.position.x = (Math.random() - 0.5) * 10;
+      donut.position.y = (Math.random() - 0.5) * 10;
+      donut.position.z = (Math.random() - 0.5) * 10;
+      donut.rotation.x = Math.random() * Math.PI;
+      donut.rotation.y = Math.random() * Math.PI;
+      const scale = Math.random();
+      donut.scale.set(scale, scale, scale);
+
+      scene.add(donut);
+    }
   });
 
   // Controls
-  const controls = new OrbitControls(camera, renderer.domElement);
+  controls = new OrbitControls(camera, renderer.domElement);
   controls.minZoom = 0.1;
   controls.maxZoom = 2;
-  controls.update();
+  controls.enableDamping = true;
 
   // Stats
   stats = Stats();
   document.body.appendChild(stats.dom);
 
   // Resize
-  window.addEventListener("resize", onWindowResize);
+  window.addEventListener('resize', onWindowResize);
 }
 
 function onWindowResize() {
@@ -121,10 +139,13 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  pointLight.position.x += 5 * Math.sin((time += 0.016));
+  const elapsedTime = clock.getElapsedTime();
+  scene.rotation.y = -elapsedTime / 10;
+
+  controls.update();
+  stats.update();
 
   render();
-  stats.update();
 }
 
 function render() {
