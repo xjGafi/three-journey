f5ef091f5cb676e353157c260bfdfa2dea558db4
Vincent <vincent.xj@outlook.com>
2023-10-18 10:04:48 +0800

    feat: 优化文字生成逻辑

--- design/src/views/no004/index.ts
+++ design/src/views/no004/index.ts
@@ -3,6 +3,7 @@ import {
   ClampToEdgeWrapping,
   // Clock,
   Color,
+  Group,
   LinearFilter,
   Mesh,
   PerspectiveCamera,
@@ -15,13 +16,15 @@ import {
   WebGLRenderer,
 } from 'three'
 
-import vertexShader from './shader/vertex.glsl?raw'
-import fragmentShader from './shader/fragment.glsl?raw'
-import vertexShaderCenterPiece from './shader/vertexShaderCenterPiece.glsl?raw'
-import fragmentShaderCenterPiece from './shader/fragmentShaderCenterPiece.glsl?raw'
+import { words } from './static'
+
+import image from '@/textures/Einstein.jpg?url'
 import snoiseShader from '@/shaders/snoise.glsl?raw'
 import circleShader from '@/shaders/circle.glsl?raw'
-import image from '@/textures/Einstein.jpg?url'
+import vertexShaderCenterPiece from './shader/vertexShaderCenterPiece.glsl?raw'
+import fragmentShaderCenterPiece from './shader/fragmentShaderCenterPiece.glsl?raw'
+import vertexShader from './shader/vertex.glsl?raw'
+import fragmentShader from './shader/fragment.glsl?raw'
 
 let camera: PerspectiveCamera, scene: Scene, renderer: WebGLRenderer
 
@@ -59,7 +62,7 @@ function init() {
 
   // Object
   createMesh()
-  createText()
+  createWords()
 
   // Renderer
   const canvas = document.querySelector('canvas#webgl')!
@@ -86,211 +89,6 @@ function animate() {
   updateView()
 
   render()
-}
-
-const wordTextureStore: any = {}
-function generateWordTexture(word: string, size: number, fontFamily: string, weight: number) {
-  if (!wordTextureStore[word]) {
-    const canvas = document.createElement('canvas')
-    const context = canvas.getContext('2d', { alpha: false })!
-    const font =
-      weight + ' ' + size * 2 + "px '" + fontFamily + "'"
-    context.font = font
-    const wordWidth = Math.max(
-      size * 1.4,
-      context.measureText(word).width
-    )
-
-    canvas.width = wordWidth + 20
-    canvas.height = wordWidth + 20
-
-    context.fillStyle = '#000'
-    context.fillRect(0, 0, wordWidth, wordWidth)
-
-    context.fillStyle = '#fff'
-    context.font = font
-    context.textAlign = 'center'
-    context.textBaseline = 'middle'
-    context.fillText(
-      word,
-      canvas.width / 2,
-      canvas.height / 2
-    )
-
-    wordTextureStore[word] = canvas
-  }
-
-  return wordTextureStore[word]
-}
-
-function createText() {
-  const wordTexture1 = new CanvasTexture(
-    generateWordTexture('G', 80, 'serif', 700)
-  )
-  wordTexture1.generateMipmaps = false
-  wordTexture1.wrapS = wordTexture1.wrapT =
-    ClampToEdgeWrapping
-  wordTexture1.minFilter = LinearFilter
-
-  const wordTexture2 = new CanvasTexture(
-    generateWordTexture('E', 80, 'serif', 700)
-  )
-  wordTexture2.generateMipmaps = false
-  wordTexture2.wrapS = wordTexture2.wrapT =
-    ClampToEdgeWrapping;
-  wordTexture2.minFilter = LinearFilter;
-
-  const wordTexture3 = new CanvasTexture(
-    generateWordTexture('N', 80, 'serif', 700)
-  );
-  wordTexture3.generateMipmaps = false;
-  wordTexture3.wrapS = wordTexture3.wrapT =
-    ClampToEdgeWrapping;
-  wordTexture3.minFilter = LinearFilter;
-
-  const wordTexture4 = new CanvasTexture(
-    generateWordTexture('I', 80, 'serif', 700)
-  );
-  wordTexture4.generateMipmaps = false;
-  wordTexture4.wrapS = wordTexture4.wrapT =
-    ClampToEdgeWrapping;
-  wordTexture4.minFilter = LinearFilter;
-
-  const wordTexture5 = new CanvasTexture(
-    generateWordTexture('U', 80, 'serif', 700)
-  );
-  wordTexture5.generateMipmaps = false;
-  wordTexture5.wrapS = wordTexture5.wrapT =
-    ClampToEdgeWrapping;
-  wordTexture5.minFilter = LinearFilter;
-
-  const wordTexture6 = new CanvasTexture(
-    generateWordTexture('S', 80, 'serif', 700)
-  );
-  wordTexture6.generateMipmaps = false;
-  wordTexture6.wrapS = wordTexture6.wrapT =
-    ClampToEdgeWrapping;
-  wordTexture6.minFilter = LinearFilter;
-
-  const geometry1 = new PlaneGeometry(
-    wordTexture1.image.width * 0.074,
-    wordTexture1.image.height * 0.074
-  );
-  const material1 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture1 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh1 = new Mesh(geometry1, material1);
-  mesh1.position.set(-29.3, 0.0, 33.0);
-  mesh1.scale.multiplyScalar(0.78);
-  scene.add(mesh1);
-
-  const geometry2 = new PlaneGeometry(
-    wordTexture2.image.width * 0.074,
-    wordTexture2.image.height * 0.074
-  );
-  const material2 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture2 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh2 = new Mesh(geometry2, material2);
-  mesh2.position.set(-19.2, 0.0, 22.0);
-  mesh2.scale.multiplyScalar(0.85);
-  scene.add(mesh2);
-
-  const geometry3 = new PlaneGeometry(
-    wordTexture3.image.width * 0.074,
-    wordTexture3.image.height * 0.074
-  );
-  const material3 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture3 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh3 = new Mesh(geometry3, material3);
-  mesh3.position.set(-6.3, 0.0, 24.0);
-  mesh3.scale.multiplyScalar(0.84);
-  scene.add(mesh3);
-
-  const geometry4 = new PlaneGeometry(
-    wordTexture4.image.width * 0.074,
-    wordTexture4.image.height * 0.074
-  );
-  const material4 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture4 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh4 = new Mesh(geometry4, material4);
-  mesh4.position.set(6.0, 0.0, 30.0);
-  mesh4.scale.multiplyScalar(0.8);
-  scene.add(mesh4);
-
-  const geometry5 = new PlaneGeometry(
-    wordTexture5.image.width * 0.074,
-    wordTexture5.image.height * 0.074
-  );
-  const material5 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture5 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh5 = new Mesh(geometry5, material5);
-  mesh5.position.set(18.6, 0.0, 26.0);
-  mesh5.scale.multiplyScalar(0.83);
-  scene.add(mesh5);
-
-  const geometry6 = new PlaneGeometry(
-    wordTexture6.image.width * 0.074,
-    wordTexture6.image.height * 0.074
-  );
-  const material6 = new ShaderMaterial({
-    uniforms: {
-      uTexture: { value: wordTexture6 },
-      uColor: {
-        value: new Color('rgb(255, 255, 255)'),
-      },
-    },
-    fragmentShader: fragmentShader,
-    vertexShader: vertexShader,
-    transparent: true,
-  });
-  const mesh6 = new Mesh(geometry6, material6);
-  mesh6.position.set(32.3, 0.0, 21.0);
-  mesh6.scale.multiplyScalar(0.86);
-  scene.add(mesh6);
 }
 
 function createMesh() {
@@ -342,6 +140,94 @@ function createMesh() {
   meshCenterPiece.position.set(0, -5, 0)
   meshCenterPiece.scale.multiplyScalar(2.6)
   scene.add(meshCenterPiece)
+}
+
+const wordTextureStore: Record<string, HTMLCanvasElement> = {}
+
+function generateWordTexture(options: {
+  word: string,
+  size?: number,
+  fontFamily?: string,
+  weight?: number
+}) {
+  const {
+    word,
+    size = 80, // 默认字体大小为 80
+    fontFamily = "serif", // 默认字体为 serif
+    weight = 700 // 默认字体粗细为 700
+  } = options;
+
+  if (!wordTextureStore[word]) {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d', { alpha: false })!
+    const font =
+      weight + ' ' + size * 2 + "px '" + fontFamily + "'"
+    context.font = font
+    const wordWidth = Math.max(
+      size * 1.4,
+      context.measureText(word).width
+    )
+
+    canvas.width = wordWidth + 20
+    canvas.height = wordWidth + 20
+
+    context.fillStyle = '#000'
+    context.fillRect(0, 0, wordWidth, wordWidth)
+
+    context.fillStyle = '#fff'
+    context.font = font
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.fillText(
+      word,
+      canvas.width / 2,
+      canvas.height / 2
+    )
+
+    wordTextureStore[word] = canvas
+  }
+
+  return wordTextureStore[word]
+}
+
+function createWords() {
+  const wordsGroup = new Group()
+
+  words.forEach(item => {
+    const { word, widthScalar, heightScalar, color, position: { x, y, z }, multiplyScalar } = item
+    const wordTexture = new CanvasTexture(
+      generateWordTexture({ word })
+    )
+    wordTexture.generateMipmaps = false
+    wordTexture.wrapS = wordTexture.wrapT =
+      ClampToEdgeWrapping
+    wordTexture.minFilter = LinearFilter
+
+    const geometry = new PlaneGeometry(
+      wordTexture.image.width * widthScalar,
+      wordTexture.image.height * heightScalar
+    );
+
+    const material = new ShaderMaterial({
+      uniforms: {
+        uTexture: { value: wordTexture },
+        uColor: {
+          value: new Color(color),
+        },
+      },
+      fragmentShader: fragmentShader,
+      vertexShader: vertexShader,
+      transparent: true,
+    });
+
+    const mesh = new Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    mesh.scale.multiplyScalar(multiplyScalar);
+
+    wordsGroup.add(mesh)
+  })
+
+  scene.add(wordsGroup);
 }
 
 function onResize() {
---
+++ design/src/views/no004/static.ts
@@ -0,0 +1,74 @@
+export const words = [
+  {
+    word: 'G',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: -29.3,
+      y: 0.0,
+      z: 33.0
+    },
+    multiplyScalar: 0.78,
+  },
+  {
+    word: 'E',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: -19.2,
+      y: 0.0,
+      z: 22.0
+    },
+    multiplyScalar: 0.85,
+  },
+  {
+    word: 'N',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: -6.3,
+      y: 0.0,
+      z: 24.0
+    },
+    multiplyScalar: 0.84,
+  },
+  {
+    word: 'I',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: 6.0,
+      y: 0.0,
+      z: 30.0
+    },
+    multiplyScalar: 0.8,
+  },
+  {
+    word: 'U',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: 18.6,
+      y: 0.0,
+      z: 26.0
+    },
+    multiplyScalar: 0.83,
+  },
+  {
+    word: 'S',
+    widthScalar: 0.074,
+    heightScalar: 0.074,
+    color: 0xffffff,
+    position: {
+      x: 32.3,
+      y: 0.0,
+      z: 21.0
+    },
+    multiplyScalar: 0.86,
+  }
+]
